29a5744d857eb7fef9a4644517fdbd4178a3f4ca
aiyoudiao <newdiao@163.com>
2023-02-14 03:03:42 +0800

    chore: update img.

--- main.js
+++ main.js
@@ -13,46 +13,46 @@ document.querySelector("#app").innerHTML = `
 
 <main>
 	<div class="card">
-		<div class="card-img"><img src="https://source.unsplash.com/900x900/?love&214" alt=""></div>
-		<div class="card-img"><img src="https://source.unsplash.com/900x900/?love&2142" alt=""></div>
-		<div class="card-img"><img src="https://source.unsplash.com/900x900/?love&21422" alt=""></div>
-		<div class="card-img"><img src="https://source.unsplash.com/900x900/?love&214222" alt=""></div>
-		<div class="card-text">Space</div>
+		<div class="card-img"><img src="https://cdn.vectorstock.com/img/pages/home/gallery/valentines/3736802.jpg" alt=""></div>
+		<div class="card-img"><img src="https://cdn.vectorstock.com/img/pages/home/gallery/valentines/3737083.jpg" alt=""></div>
+		<div class="card-img"><img src="https://cdn.vectorstock.com/img/pages/home/gallery/valentines/1909080.jpg" alt=""></div>
+		<div class="card-img"><img src="https://cdn.vectorstock.com/img/pages/home/gallery/valentines/1837606.jpg" alt=""></div>
+		<div class="card-text">仪式感</div>
 	</div>
 	<div class="card">
 		<div class="card-img">
-			<img src="https://source.unsplash.com/320x800/?love&2142222" alt="">
+			<img src="https://cdn.vectorstock.com/img/pages/home/gallery/valentines/3605061.jpg" alt="">
 		</div>
 		<div class="card-img">
-			<img src="https://source.unsplash.com/320x800/?love&21422222" alt="">
+			<img src="https://cdn.vectorstock.com/img/pages/home/gallery/valentines/3728195.jpg" alt="">
 		</div>
-		<div class="card-text">Sea</div>
+		<div class="card-text">嬉皮</div>
 	</div>
 	<div class="card">
 		<div class="card-img">
-			<img src="https://source.unsplash.com/900x900/?love&21422222222222" alt="">
+			<img src="https://cdn.vectorstock.com/i/preview-2x/85/17/tunnel-of-concentric-hearts-background-vector-44898517.webp" alt="">
 		</div>
-		<div class="card-text">Fish</div>
+		<div class="card-text">浪漫</div>
 	</div>
 	<div class="card">
 		<div class="card-img">
-			<img src="https://source.unsplash.com/900x900/?love&2142222222" alt="">
-		</div>
-	</div>
-	<div class="card">
-		<div class="card-img">
-			<img src="https://source.unsplash.com/700x100/?love&21422222222" alt="">
-		</div>
-		<div class="card-img">
-			<img src="https://source.unsplash.com/700x100/?love&214222222222" alt="">
+			<img src="https://cdn5.vectorstock.com/i/1000x1000/75/29/happy-valentines-day-vector-34227529.jpg" alt="">
 		</div>
 	</div>
 	<div class="card">
 		<div class="card-img">
-			<img src="https://source.unsplash.com/340x220/?love&2142222222222" alt="">
+			<img src="https://cdn.vectorstock.com/i/preview-2x/20/84/valentines-day-banner-template-vector-45702084.webp" alt="">
 		</div>
 		<div class="card-img">
-			<img src="https://source.unsplash.com/340x220/?love&21422222222222" alt="">
+			<img src="https://cdn.vectorstock.com/i/preview-2x/02/34/beautiful-red-3d-hearts-valentines-day-banner-vector-45900234.webp" alt="">
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-img">
+			<img src="https://cdn.vectorstock.com/i/preview-2x/32/76/valentines-day-greeting-card-vector-40893276.webp" alt="">
+		</div>
+		<div class="card-img">
+			<img src="https://cdn3.vectorstock.com/i/1000x1000/66/52/valentines-day-smoothie-vector-45466652.jpg" alt="">
 		</div>
 	</div>
 </main>
